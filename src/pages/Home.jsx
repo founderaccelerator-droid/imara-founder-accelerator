1478560b0add582628ed65ef3c40fa4786ba93ab
founderaccelerator-droid <founderaccelerator@gmail.com>
2026-04-07 20:24:36 +0200

    Update Home.jsx
    Integrate WhyUs component in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,3 +24,19 @@ const Home = () => {
 };
 
 export default Home;
+import React from "react";
+import Hero from "../components/home/Hero";
+import Programs from "../components/home/Programs";
+import WhyUs from "../components/home/WhyUs";  // <-- import ajouté
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Programs />
+      <WhyUs />  // <-- ajouté ici
+    </div>
+  );
+};
+
+export default Home;
