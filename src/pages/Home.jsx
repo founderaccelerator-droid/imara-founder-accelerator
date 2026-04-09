4f161d5248862668d62bfe5a9a360b5e20c6f88d
founderaccelerator-droid <founderaccelerator@gmail.com>
2026-04-09 19:10:02 +0200

    Update Home.jsx
    git add src/pages/Home.jsx
    git commit -m "Fix Home page structure and clean duplicated components"
    git push origin main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,77 +1,25 @@
 import React from "react";
-import Hero from "../components/home/Hero";
 
-const Home = () => {
-  return (
-    <div>
-      <Hero />
-    </div>
-  );
-};
-
-export default Home;
-import React from "react";
 import Hero from "../components/home/Hero";
 import Programs from "../components/home/Programs";
-
-const Home = () => {
-  return (
-    <div>
-      <Hero />
-      <Programs />
-    </div>
-  );
-};
-
-export default Home;
-import React from "react";
-import Hero from "../components/home/Hero";
-import Programs from "../components/home/Programs";
-import WhyUs from "../components/home/WhyUs";  // <-- import ajouté
-
-const Home = () => {
-  return (
-    <div>
-      <Hero />
-      <Programs />
-      <WhyUs />  // <-- ajouté ici
-    </div>
-  );
-};
-
-
+import WhyUs from "../components/home/WhyUs";
 import QuickApply from "../components/home/QuickApply";
-      <div>
-  <Hero />
-  <Programs />
-  <WhyUs />
-  <QuickApply />  // <-- ajouté ici
-</div>
 import Testimonials from "../components/home/Testimonials";
-        <div>
-  <Hero />
-  <Programs />
-  <WhyUs />
-  <QuickApply />
-  <Testimonials />  // <-- ajouté ici
-</div>
 import AlumniGallery from "../components/home/AlumniGallery";
-          <div>
-  <Hero />
-  <Programs />
-  <WhyUs />
-  <QuickApply />
-  <Testimonials />
-  <AlumniGallery />  // <-- ajouté ici
-</div>
 import Footer from "../components/home/Footer";
-            <div>
-  <Hero />
-  <Programs />
-  <WhyUs />
-  <QuickApply />
-  <Testimonials />
-  <AlumniGallery />
-  <Footer />  // <-- ajouté ici
-</div>
- export default Home;             
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Programs />
+      <WhyUs />
+      <QuickApply />
+      <Testimonials />
+      <AlumniGallery />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
